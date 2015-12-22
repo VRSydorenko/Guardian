d98efb7cbbfd08c7c9e87e486a5dcce85173d5d7
Viktor Sydorenko <viktor.sydorenko@gmail.com>
2015-12-22 13:53:14 +0100

    Add CheckVC try 2

---
+++ Guardian/Guardian/CheckVC.h
@@ -0,0 +1,13 @@
+//
+//  CheckVC.h
+//  Guardian
+//
+//  Created by Eurofunk on 22.12.15.
+//  Copyright © 2015 Viktor Sydorenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckVC : UIViewController
+
+@end
