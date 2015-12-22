de42668aec1b6f553b222a13c4faff31cfb26913
Viktor Sydorenko <viktor.sydorenko@gmail.com>
2015-12-22 10:21:35 +0100

    Point container class

---
+++ Guardian/Data/CheckPoints.h
@@ -0,0 +1,15 @@
+//
+//  CheckPoints.h
+//  Guardian
+//
+//  Created by VRS on 22/12/15.
+//  Copyright © 2015 Viktor Sydorenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CheckPoints : NSObject
+
+-(NSDictionary*)get;
+
+@end
---
+++ Guardian/Data/CheckPoints.m
@@ -0,0 +1,22 @@
+//
+//  CheckPoints.m
+//  Guardian
+//
+//  Created by VRS on 22/12/15.
+//  Copyright © 2015 Viktor Sydorenko. All rights reserved.
+//
+
+#import "CheckPoints.h"
+
+@implementation CheckPoints
+
+-(NSDictionary*)get
+{
+    NSDictionary* points = [[NSDictionary alloc] init];
+    
+    
+    
+    return points;
+}
+
+@end
