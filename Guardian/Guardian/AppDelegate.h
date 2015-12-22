55c4d95338953e70464f4f349e152a221383a43b
Viktor Sydorenko <viktor.sydorenko@gmail.com>
2015-12-22 09:18:26 +0100

    Initial commit

---
+++ Guardian/Guardian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Guardian
+//
+//  Created by VRS on 22/12/15.
+//  Copyright © 2015 Viktor Sydorenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Guardian/Guardian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Guardian
+//
+//  Created by VRS on 22/12/15.
+//  Copyright © 2015 Viktor Sydorenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
